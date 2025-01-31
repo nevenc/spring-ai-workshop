1f9446fd06d0d943874ecddf09e3b4f2389cf8c6
Timo Salm <accounts@timosalm.de>
2025-01-31 07:36:42 +0100

    Improvements

--- workshop/setup.d/01-workshop-setup.sh
+++ workshop/setup.d/01-workshop-setup.sh
@@ -3,6 +3,6 @@
 set -x
 set -eo pipefail
 
-jq ". + { \"editor.fontSize\": 14, \"files.exclude\": { \".**\": true}}" /home/eduk8s/.local/share/code-server/User/settings.json | sponge /home/eduk8s/.local/share/code-server/User/settings.json
+jq ". + { \"editor.fontSize\": 14, \"files.exclude\": { \".**\": true}}" /home/eduk8s/.local/share/code-server/User/settings.json > /home/eduk8s/.local/share/code-server/User/settings.json.tmp && mv /home/eduk8s/.local/share/code-server/User/settings.json.tmp /home/eduk8s/.local/share/code-server/User/settings.json
 
 git clone https://github.com/nevenc/spring-ai-zero-to-hero spring-ai-zero-to-hero
