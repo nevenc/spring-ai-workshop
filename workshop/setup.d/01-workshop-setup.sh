ab26f26d1a96b23f743956e390342c0720fac6ba
Neven Cvetkovic <neven.cvetkovic@gmail.com>
2025-01-31 23:11:26 +0100

    Updated git location of the lab code.

--- workshop/setup.d/01-workshop-setup.sh
+++ workshop/setup.d/01-workshop-setup.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 
 jq ". + { \"editor.fontSize\": 14, \"files.exclude\": { \".**\": true}}" /home/eduk8s/.local/share/code-server/User/settings.json > /home/eduk8s/.local/share/code-server/User/settings.json.tmp && mv /home/eduk8s/.local/share/code-server/User/settings.json.tmp /home/eduk8s/.local/share/code-server/User/settings.json
 
-git clone https://github.com/nevenc/spring-ai-zero-to-hero spring-ai-zero-to-hero
+git clone https://github.com/nevenc/spring-ai-intro
 
 code-server --install-extension redhat.java
 code-server --install-extension redhat.vscode-yaml
